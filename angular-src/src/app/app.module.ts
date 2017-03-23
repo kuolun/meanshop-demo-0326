c37e727ff5159c2658e1bd7531a0885456af2b62
kuolun <kuolun@gmail.com>
2017-03-23 14:42:05 +0800

    add 5 components

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './src/app/components/navbar/navbar.component';
+import { ProductlistComponent } from './src/app/components/productlist/productlist.component';
+import { ProductdetailComponent } from './src/app/components/productdetail/productdetail.component';
+import { ProfileComponent } from './src/app/components/profile/profile.component';
+import { CartComponent } from './src/app/components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductlistComponent,
+    ProductdetailComponent,
+    ProfileComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
