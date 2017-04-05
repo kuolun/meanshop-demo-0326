92d0fb46cbceec8734b6f2effbbec2fa231ae7fc
kuolun <kuolun@gmail.com>
2017-04-06 00:24:36 +0800

    fix countItem using let

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -8,24 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  //購物車內item數
-  itemCount = 0;
-
   constructor(private _auth: AuthService) { }
 
   ngOnInit() {
-    console.log('userProfile:', this._auth.userProfile);
   }
 
 
   countItem() {
-    // if (this._auth.userProfile.data.cart.length > 0) {
-    //   //加總購物車item數
-    //   this._auth.userProfile.data.cart.forEach(item => {
-    //     this.itemCount += item.quantity;
-    //   });
-    // }
-    return this.itemCount;
+    let itemCount = 0;
+    //加總購物車item數
+    this._auth.userProfile.data.cart.forEach(item => {
+      itemCount += item.quantity;
+    });
+    return itemCount;
   }
 
 }
